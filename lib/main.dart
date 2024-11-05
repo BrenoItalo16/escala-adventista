ad734015cd2e4b8499551b2704e74cea1be9765f
Breno Italo <srbrenoitalo16@gmail.com>
2024-11-04 23:19:57 -0300

    feat: App Theme initializes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'core/app_widget.dart';
+import 'app/app_widget.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   return runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -1,3 +1,4 @@
+import 'package:escala_adventista/app/ui/pages/onboarding/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -8,6 +9,10 @@ final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
+      builder: (context, state) => SplashPage(),
+    ),
+    GoRoute(
+      path: '/auth',
       builder: (context, state) => Scaffold(
         appBar: AppBar(
           title: Text(AppLocalizations.of(context)!.title),
