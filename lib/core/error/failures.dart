1d720c2abdf904cd08be9f3be461236b397b421d
BrenoItalo16 <contato@brenoitalo.com>
2024-12-05 00:23:53 -0300

    fix: Fixing problems messages

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -2,20 +2,23 @@ import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
   final String message;
-  const Failure(this.message);
+  const Failure({required this.message});
 
   @override
   List<Object> get props => [message];
 }
 
 class AuthFailure extends Failure {
-  const AuthFailure([String message = 'Erro de autenticação']) : super(message);
+  const AuthFailure([String message = 'Erro de autenticação'])
+      : super(message: message);
 }
 
 class CacheFailure extends Failure {
-  const CacheFailure([String message = 'Erro de cache']) : super(message);
+  const CacheFailure([String message = 'Erro de cache'])
+      : super(message: message);
 }
 
 class ServerFailure extends Failure {
-  const ServerFailure([String message = 'Erro no servidor']) : super(message);
+  const ServerFailure([String message = 'Erro no servidor'])
+      : super(message: message);
 }
